74b150ddab11032d7e6922431db199caf7eb381b
Santiago Dueñas <sduenas@bitergia.com>
2017-05-11 20:11:35 +0200

    [datetime] Add module for managing datatime objects
    This module contains functions to parse datetimes from
    strings, to set default timezones or to convert dates
    in different time formats.
    
    The functions on this module were originally developed
    in Perceval tool.

---
+++ tests/test_datetime.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2017 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+import datetime
+import sys
+import unittest
+
+import dateutil
+import pkg_resources
+
+sys.path.insert(0, '..')
+pkg_resources.declare_namespace('grimoirelab.toolkit')
+
+from grimoirelab.toolkit.datetime import (InvalidDateError,
+                                          datetime_to_utc,
+                                          str_to_datetime,
+                                          unixtime_to_datetime)
+
+
+class TestInvalidDateError(unittest.TestCase):
+
+    def test_message(self):
+        """Make sure that prints the correct error"""
+
+        e = InvalidDateError(date='1900-13-01')
+        self.assertEqual('1900-13-01 is not a valid date',
+                         str(e))
+
+
+class TestDatetimeToUTC(unittest.TestCase):
+    """Unit tests for datetime_to_utc function."""
+
+    def test_conversion(self):
+        """Check if it converts some timestamps to timestamps with UTC+0."""
+
+        date = datetime.datetime(2001, 12, 1, 23, 15, 32,
+                                 tzinfo=dateutil.tz.tzoffset(None, -21600))
+        expected = datetime.datetime(2001, 12, 2, 5, 15, 32,
+                                     tzinfo=dateutil.tz.tzutc())
+        utc = datetime_to_utc(date)
+        self.assertIsInstance(utc, datetime.datetime)
+        self.assertEqual(utc, expected)
+
+        date = datetime.datetime(2001, 12, 1, 23, 15, 32,
+                                 tzinfo=dateutil.tz.tzutc())
+        expected = datetime.datetime(2001, 12, 1, 23, 15, 32,
+                                     tzinfo=dateutil.tz.tzutc())
+        utc = datetime_to_utc(date)
+        self.assertIsInstance(utc, datetime.datetime)
+        self.assertEqual(utc, expected)
+
+        date = datetime.datetime(2001, 12, 1, 23, 15, 32)
+        expected = datetime.datetime(2001, 12, 1, 23, 15, 32,
+                                     tzinfo=dateutil.tz.tzutc())
+        utc = datetime_to_utc(date)
+        self.assertIsInstance(utc, datetime.datetime)
+        self.assertEqual(utc, expected)
+
+    def test_invalid_datetime(self):
+        """Check if it raises an exception on invalid instances."""
+
+        self.assertRaises(InvalidDateError, datetime_to_utc, "2016-01-01 01:00:00 +0800")
+        self.assertRaises(InvalidDateError, datetime_to_utc, None)
+        self.assertRaises(InvalidDateError, datetime_to_utc, 1)
+
+
+class TestStrToDatetime(unittest.TestCase):
+    """Unit tests for str_to_datetime function."""
+
+    def test_dates(self):
+        """Check if it converts some dates to datetime objects."""
+
+        date = str_to_datetime('2001-12-01')
+        expected = datetime.datetime(2001, 12, 1, tzinfo=dateutil.tz.tzutc())
+        self.assertIsInstance(date, datetime.datetime)
+        self.assertEqual(date, expected)
+
+        date = str_to_datetime('13-01-2001')
+        expected = datetime.datetime(2001, 1, 13, tzinfo=dateutil.tz.tzutc())
+        self.assertIsInstance(date, datetime.datetime)
+        self.assertEqual(date, expected)
+
+        date = str_to_datetime('12-01-01')
+        expected = datetime.datetime(2001, 12, 1, tzinfo=dateutil.tz.tzutc())
+        self.assertIsInstance(date, datetime.datetime)
+        self.assertEqual(date, expected)
+
+        date = str_to_datetime('2001-12-01 23:15:32')
+        expected = datetime.datetime(2001, 12, 1, 23, 15, 32,
+                                     tzinfo=dateutil.tz.tzutc())
+        self.assertIsInstance(date, datetime.datetime)
+        self.assertEqual(date, expected)
+
+        date = str_to_datetime('2001-12-01 23:15:32 -0600')
+        expected = datetime.datetime(2001, 12, 1, 23, 15, 32,
+                                     tzinfo=dateutil.tz.tzoffset(None, -21600))
+        self.assertIsInstance(date, datetime.datetime)
+        self.assertEqual(date, expected)
+
+        date = str_to_datetime('2001-12-01 23:15:32Z')
+        expected = datetime.datetime(2001, 12, 1, 23, 15, 32,
+                                     tzinfo=dateutil.tz.tzutc())
+        self.assertIsInstance(date, datetime.datetime)
+        self.assertEqual(date, expected)
+
+        date = str_to_datetime('Wed, 26 Oct 2005 15:20:32 -0100 (GMT+1)')
+        expected = datetime.datetime(2005, 10, 26, 15, 20, 32,
+                                     tzinfo=dateutil.tz.tzoffset(None, -3600))
+        self.assertIsInstance(date, datetime.datetime)
+        self.assertEqual(date, expected)
+
+        date = str_to_datetime('Wed, 22 Jul 2009 11:15:50 +0300 (FLE Daylight Time)')
+        expected = datetime.datetime(2009, 7, 22, 11, 15, 50,
+                                     tzinfo=dateutil.tz.tzoffset(None, 10800))
+        self.assertIsInstance(date, datetime.datetime)
+        self.assertEqual(date, expected)
+
+        date = str_to_datetime('Thu, 14 Aug 2008 02:07:59 +0200 CEST')
+        expected = datetime.datetime(2008, 8, 14, 2, 7, 59,
+                                     tzinfo=dateutil.tz.tzoffset(None, 7200))
+        self.assertIsInstance(date, datetime.datetime)
+        self.assertEqual(date, expected)
+
+        date = str_to_datetime('Thu, 14 Aug 2008 02:07:59 +0200 +0100')
+        expected = datetime.datetime(2008, 8, 14, 2, 7, 59,
+                                     tzinfo=dateutil.tz.tzoffset(None, 7200))
+        self.assertIsInstance(date, datetime.datetime)
+        self.assertEqual(date, expected)
+
+        # This date is invalid because the timezone section.
+        # Timezone will be removed, setting UTC as default
+        date = str_to_datetime('2001-12-01 02:00 +08888')
+        expected = datetime.datetime(2001, 12, 1, 2, 0, 0,
+                                     tzinfo=dateutil.tz.tzutc())
+        self.assertIsInstance(date, datetime.datetime)
+        self.assertEqual(date, expected)
+
+    def test_invalid_date(self):
+        """Check whether it fails with an invalid date."""
+
+        self.assertRaises(InvalidDateError, str_to_datetime, '2001-13-01')
+        self.assertRaises(InvalidDateError, str_to_datetime, '2001-04-31')
+
+    def test_invalid_format(self):
+        """Check whether it fails with invalid formats."""
+
+        self.assertRaises(InvalidDateError, str_to_datetime, '2001-12-01mm')
+        self.assertRaises(InvalidDateError, str_to_datetime, 'nodate')
+        self.assertRaises(InvalidDateError, str_to_datetime, None)
+        self.assertRaises(InvalidDateError, str_to_datetime, '')
+
+
+class TestUnixTimeToDatetime(unittest.TestCase):
+    """Unit tests for str_to_datetime function."""
+
+    def test_dates(self):
+        """Check if it converts some timestamps to datetime objects."""
+
+        date = unixtime_to_datetime(0)
+        expected = datetime.datetime(1970, 1, 1, 0, 0, 0,
+                                     tzinfo=dateutil.tz.tzutc())
+        self.assertIsInstance(date, datetime.datetime)
+        self.assertEqual(date, expected)
+
+        date = unixtime_to_datetime(1426868155.0)
+        expected = datetime.datetime(2015, 3, 20, 16, 15, 55,
+                                     tzinfo=dateutil.tz.tzutc())
+        self.assertIsInstance(date, datetime.datetime)
+        self.assertEqual(date, expected)
+
+    def test_invalid_format(self):
+        """Check whether it fails with invalid formats."""
+
+        self.assertRaises(InvalidDateError, str_to_datetime, '2001-12-01mm')
+        self.assertRaises(InvalidDateError, str_to_datetime, 'nodate')
+        self.assertRaises(InvalidDateError, str_to_datetime, None)
+        self.assertRaises(InvalidDateError, str_to_datetime, '')
+
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
