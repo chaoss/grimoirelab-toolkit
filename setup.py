b50f76df0b9b27aac3b3e58f3323f90ad5add7c7
Santiago Dueñas <sduenas@bitergia.com>
2017-05-12 12:25:57 +0200

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2017 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#     Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
+#
+
+import codecs
+import os.path
+import re
+
+from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme_md = os.path.join(here, 'README.md')
+version_py = os.path.join(here, 'grimoirelab', 'toolkit', '_version.py')
+
+# Pypi wants the description to be in reStrcuturedText, but
+# we have it in Markdown.
+try:
+    import pypandoc
+    long_description = pypandoc.convert(readme_md, 'rst')
+except (IOError, ImportError):
+    print("Warning: pypandoc module not found, or pandoc not installed. "
+          "Using md instead of rst")
+    with codecs.open(readme_md, encoding='utf-8') as f:
+        long_description = f.read()
+
+with codecs.open(version_py, 'r', encoding='utf-8') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+
+setup(name="grimoirelab-toolkit",
+      description="Toolkit of common functions used across GrimoireLab",
+      long_description=long_description,
+      url="https://github.com/grimoirelab/grimoirelab-toolkit",
+      version=version,
+      author="Bitergia",
+      author_email="sduenas@bitergia.com",
+      license="GPLv3",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Programming Language :: Python :: 3'
+      ],
+      keywords="development grimoirelab",
+      packages=[
+          'grimoirelab',
+          'grimoirelab.toolkit',
+      ],
+      namespaces=[
+          'grimoirelab.toolkit'
+      ],
+      install_requires=[
+          'python-dateutil>=2.6.0'
+      ],
+      zip_safe=False)
