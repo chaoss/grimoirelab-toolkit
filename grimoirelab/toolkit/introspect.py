b1cf6fa89c5046ae2e47cd62cd816f9dbdc5313f
Valerio Cosentino <valcos@bitergia.com>
2018-01-23 11:45:20 +0100

    [introspect] Ignore * and ** parameters in method signatures
    This code allows to ignore * and ** parameters of the fetch method
    in backend.py

--- grimoirelab/toolkit/introspect.py
+++ grimoirelab/toolkit/introspect.py
@@ -81,15 +81,21 @@ def find_signature_parameters(callable_, candidates,
                                                     excluded=excluded)
     exec_params = {}
 
+    add_all = False
     for param in signature_params:
         name = param.name
 
-        if name in candidates:
+        if str(param).startswith('*'):
+            add_all = True
+        elif name in candidates:
             exec_params[name] = candidates[name]
         elif param.default == inspect.Parameter.empty:
             msg = "required argument %s not found" % name
             raise AttributeError(msg, name)
         else:
             continue
+
+    if add_all:
+        exec_params = candidates
 
     return exec_params
